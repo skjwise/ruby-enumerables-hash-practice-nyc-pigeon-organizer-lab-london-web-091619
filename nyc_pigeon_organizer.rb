aad16b4fec01d9a7d71235ab904ef56492fab4ec
Sarah Wise <skjwise@hotmail.com>
2019-08-31 13:34:23 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,5 +3,13 @@ def nyc_pigeon_organizer(data)
   data.each do |first_level, all_other|
     all_other.each do |category, array|
       array.each do |name|
-        final[name] = {:color => [], :gender => 
+        final[name] = {:color => [], :gender => [], :lives => []}
+      end
+    end
+  end
+  x = final.keys
+  data[:color].each do |bird_color, name|
+    name.each do |bird_name|
+      x.each do |item|
+        if bird_name === item
 end
