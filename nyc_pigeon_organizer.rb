5dc4e3e89e1c898211c970e453abced0947d7bf6
Sarah Wise <skjwise@hotmail.com>
2019-08-31 13:35:14 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -12,5 +12,8 @@ def nyc_pigeon_organizer(data)
     name.each do |bird_name|
       x.each do |item|
         if bird_name == item
-          final[item][:color] << bird_color.to
+          final[item][:color] << bird_color.to_s
+        end
+      end
+    end
 end
