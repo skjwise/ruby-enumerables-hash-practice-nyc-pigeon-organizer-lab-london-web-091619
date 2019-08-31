ac68c25aeab413b41e6f5f220a584d49ee644186
Sarah Wise <skjwise@hotmail.com>
2019-08-31 13:38:15 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -16,4 +16,24 @@ def nyc_pigeon_organizer(data)
         end
       end
     end
+  end
+  data[:gender].each do |gender, type|
+    type.each do |bird_name|
+      x.each do |item|
+        if bird_name == item
+          final[item][:gender] << gender.to_s
+        end
+      end
+    end
+  end
+  data[:lives].each do |location, name|
+    name.each do |bird_name|
+      x.each do |item|
+        if bird_name == item
+          final[item][:lives] << location
+        end
+      end
+    end
+  end
+  return final
 end
